240aa3ddf9015da0bf4aa6eef129937829bbf92a
James Draper <james.draper@duke.edu>
2018-04-02 17:13:53 -0400

    navigating pypi

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/draperjames/witch',
-    version="version='0.0.2'",
+    version="version='0.0.3'",
     zip_safe=False,
 )
